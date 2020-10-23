e675c6cd6bedf77f323d48de7173132c737d7316
Matt Knighten <jason.knighten@revature.com>
2020-10-23 13:02:47 -0400

    added enum demo

---
+++ Java/ProjectMayhem/src/com/revature/enums/DaysOfTheWeek.java
@@ -0,0 +1,17 @@
+package com.revature.enums;
+
+public class DaysOfTheWeek {
+	enum Days{
+		MONDAY,
+		TUESDAY,
+		WEDNESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY,
+		SUNDAY
+	}
+	
+	Days myDay= Days.FRIDAY;
+	
+	
+}
