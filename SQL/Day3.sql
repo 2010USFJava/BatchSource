9cbd952f26a3a8092579bd14842f3250faeecb94
Matt Knighten <jason.knighten@revature.com>
2020-11-11 12:48:16 -0500

    added SQL folder and .sql files for day 3

---
+++ SQL/Day3.sql
@@ -0,0 +1,38 @@
+--Single line commment
+/*
+ * multi line comment
+ */
+
+create table pookiemans(
+pid integer primary key,
+pname varchar(30)
+);
+
+insert into pookiemans values (1,'bulbasaur');
+
+select * from pookiemans;
+
+insert into pookiemans (pid) values(2);
+
+update pookiemans
+set pname='ivysaur';
+
+update pookiemans 
+set pname='bulbasaur'
+where pid=1;
+
+insert into pookiemans (pid,pname) values
+(3,'venusaur'),
+(4,'chramander'),
+(5, 'charmeleon');
+
+delete from pookiemans
+where pname='chramander';
+
+commit;
+
+delete from pookiemans;
+select * from pookiemans;
+rollback;
+
+
